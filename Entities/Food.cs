7fdf26033d97bf29ff029e412bebd683920e1867
Nazlı Merve Koç <nazlimervekilic1@gmail.com>
2023-05-07 17:26:06 +0300

    Food Entity girildi.

--- Entities/Food.cs
+++ Entities/Food.cs
@@ -8,5 +8,18 @@ namespace Entities
 {
     internal class Food
     {
+        public int FoodID { get; set; }
+        public string Name { get; set; }
+        public decimal Calori { get; set; }
+        public decimal ProteinRate { get; set; }
+        public decimal VitaminRate { get; set; }
+        public decimal FatRate { get; set; }
+        public decimal CarbonhydrateAmount { get; set; }
+
+
+        //Relations
+
+        //public ICollection<Meal_Food> Meals { get; set; }
+
     }
 }
