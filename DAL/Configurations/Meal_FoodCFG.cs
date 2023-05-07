ab70c17c7eb3d860fa6f521dbd9ad6ecf9ab5d75
Enes Kurt <eneskurt1996@hotmail.com>
2023-05-07 17:56:43 +0300

    DAL updated.

--- DAL/Configurations/Meal_FoodCFG.cs
+++ DAL/Configurations/Meal_FoodCFG.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Enums
+namespace DAL.Configurations
 {
-    public enum MealType
+    internal class Meal_FoodCFG
     {
-        Breakfast = 1,
-        Lunch,
-        Dinner,
-        Others
     }
 }
