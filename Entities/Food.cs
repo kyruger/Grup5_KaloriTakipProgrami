1a2ca2bf74e0f82c4f5834fbebd22fb2b4b85e09
Enes Kurt <eneskurt1996@hotmail.com>
2023-05-07 17:35:00 +0300

    ProjeV1 updated.

--- Entities/Food.cs
+++ Entities/Food.cs
@@ -6,20 +6,15 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    internal class Food
+    public class Food
     {
         public int FoodID { get; set; }
         public string Name { get; set; }
         public decimal Calori { get; set; }
         public decimal ProteinRate { get; set; }
-        public decimal VitaminRate { get; set; }
         public decimal FatRate { get; set; }
         public decimal CarbonhydrateAmount { get; set; }
-
-
-        //Relations
-
-        //public ICollection<Meal_Food> Meals { get; set; }
+        public ICollection<Meal_Food> Meals { get; set; }
 
     }
 }
--- Entities/Meal.cs
+++ Entities/Meal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,8 @@ namespace Entities
     public class Meal
     {
         public int MealId { get; set; }
-        public string MealType { get; set; }
+        public MealType MealType { get; set; }
         public int Day { get; set; }
-
         public ICollection<Meal_Food> Foods { get; set; }
     }
 }
