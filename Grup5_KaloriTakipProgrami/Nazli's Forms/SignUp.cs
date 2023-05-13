aa1c25718bc96b84a86cc5263d5fdb31ae109429
Nazlı Merve Koç <nazlimervekilic1@gmail.com>
2023-05-13 14:22:14 +0300

    codes added.

--- Grup5_KaloriTakipProgrami/Nazli's Forms/SignUp.cs
+++ Grup5_KaloriTakipProgrami/Nazli's Forms/SignUp.cs
@@ -32,25 +32,35 @@ namespace WndPL.Forms
         }
         private void btnConfirm_Click(object sender, EventArgs e)
         {
-            var asdas = this;
-            bool emptyControl = helper.AreTextBoxesEmpty(this);
-            if (emptyControl)
+            if (AreFieldsEmpty())
             {
-                MessageBox.Show("Eksik bilgi girdiniz. \nLütfen tüm alanları doldurunuz.");
+                MessageBox.Show("Alanlar boş geçilemez. Lütfen eksik bilgilerinizi giriniz.");
+            }
+
+            if (ContainsNumeric(txtFirstName.Text) || ContainsNumeric(txtLastName.Text))
+            {
+                MessageBox.Show("İsim ve soyisimde nümerik değer giremezsiniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+
+            if ((txtFirstName.Text).Length < 2 || (txtLastName.Text).Length < 2)
+            {
+                MessageBox.Show("İsim ve soyisim en az 2 harf içermelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             if (txtMail.Text != txtMailRepeat.Text)
             {
-                MessageBox.Show("Girdiğiniz mail adresleri uyuşmuyor.");
+                MessageBox.Show("Girdiğiniz mail adresleri uyuşmuyor.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else if (!IsPasswordValid(txtPassword.Text))
             {
-                MessageBox.Show("Şifreler uygun kriterlere sahip değil\n---Kriterler---\n•En az 8 karakter uzunluğunda olmalıdır.\n• En az 2 büyük harf içermelidir.\n• En az 3 küçük harf içermelidir.\n• ! (ünlem), : (iki nokta üst üste), +(artı), *(yıldız) karakterlerinden en az 2 tanesini içermelidir.");
+                MessageBox.Show("Şifreler uygun kriterlere sahip değil\n---Kriterler---\n•En az 8 karakter uzunluğunda olmalıdır.\n• En az 2 büyük harf içermelidir.\n• En az 3 küçük harf içermelidir.\n• ! (ünlem), : (iki nokta üst üste), +(artı), *(yıldız) karakterlerinden en az 2 tanesini içermelidir.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             }
             else if (txtPassword.Text != txtPasswordRepeat.Text)
             {
-                MessageBox.Show("Şifreler eşleşmiyor. \nLütfen şifreleri kontrol ediniz.");
+                MessageBox.Show("Şifreler eşleşmiyor. \nLütfen şifreleri kontrol ediniz.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
             else
@@ -86,6 +96,37 @@ namespace WndPL.Forms
             if (password.Count(c => "!:+*".Contains(c)) < minSpecialChars) { return false; }
             return true;
         }
+
+        private bool ContainsNumeric(string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool AreFieldsEmpty()
+        {
+            if (string.IsNullOrWhiteSpace(txtMail.Text) ||
+                string.IsNullOrWhiteSpace(txtMailRepeat.Text) ||
+                string.IsNullOrWhiteSpace(txtPassword.Text) ||
+                string.IsNullOrWhiteSpace(txtPasswordRepeat.Text) ||
+                string.IsNullOrWhiteSpace(txtFirstName.Text) ||
+                string.IsNullOrWhiteSpace(txtLastName.Text)
+               )
+
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
 
         private void guna2PictureBox3_Click(object sender, EventArgs e)
         {
